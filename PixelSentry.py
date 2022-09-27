1d4c9aa9715023c7250c6dfbd874e7d47ffdc0f1
YSpiegel <spiegel.yoav@gmail.com>
2022-09-27 15:27:10 +0300

    Bug fixes, difficulty raised

--- PixelSentry.py
+++ PixelSentry.py
@@ -30,8 +30,19 @@ def get_angle(pos1, pos2):
     :param pos2: second input position
     :return: angle between pos1 and pos2
     """
-    angle = math.atan(- (pos2[1] - pos1[1]) / (pos2[0] - pos1[0])) if pos2[0] != pos1[0] else math.pi / 2
-    return angle if pos2[0] > pos1[0] else math.pi + angle
+    x1, y1, x2, y2 = pos1[0], pos1[1], pos2[0], pos2[1]
+    if x2 != x1:
+        angle = math.atan(- (y2 - y1) / (x2 - x1))
+    else:
+        if pos2[1] > pos1[1]:
+            return 3 * math.pi / 2
+        else:
+            return math.pi / 2
+
+    if pos2[0] > pos1[0]:
+        return angle
+    else:
+        return math.pi + angle
 
 
 def end_of_round():
@@ -67,6 +78,7 @@ def game():
 
     # pixels properties
     pixels = []
+    total_pixels = 0
     last_attack = time.time()
 
     # shots properties
@@ -99,16 +111,19 @@ def game():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.MOUSEBUTTONDOWN:
-                shots.append([sentry_angle, 16])
+                if len(shots) == 0 or shots[-1][1] > 12:
+                    shots.append([sentry_angle, 16])
 
         # set sentry angle to mouse position
-        sentry_angle = get_angle([300, 300], pygame.mouse.get_pos())
+        if len(shots) == 0 or shots[-1][1] > 12:
+            sentry_angle = get_angle([300, 300], pygame.mouse.get_pos())
 
         # shoot a pixel if time has passed
-        if time.time() - last_attack > 1.5 + random.random():
+        if time.time() - last_attack > 0.1 + 1 / (0.03 * total_pixels + 0.5) + random.random():
             pixels.append([random.choice(numpy.arange(math.pi * 5 / 8, math.pi * 2 + math.pi * 3 / 8, math.pi / 32)),
                            300])
             last_attack = time.time()
+            total_pixels += 1
 
         dis.fill((255, 0, 0))
 
@@ -123,13 +138,13 @@ def game():
             bottomx = 310 + radius * math.cos(angle)
             bottomy = 310 - radius * math.sin(angle)
             pygame.draw.rect(dis, black, [300 + radius * math.cos(angle), 300 - radius * math.sin(angle), 10, 10])
-            pixel[1] -= 0.05
+            pixel[1] -= 0.05 + 0.002 * total_pixels / 10
             for shot in shots:
                 if topx <= 300 + shot[1] * math.cos(shot[0]) <= bottomx \
-                        and topy <= 300 - shot[1] * math.sin(shot[0]) <= bottomy:
-                    pixels.remove(pixel)
-                    shots.remove(shot)
-                    score += 1
+                        and topy <= 300 - shot[1] * math.sin(shot[0]) <= bottomy and pixel in pixels:
+                        pixels.remove(pixel)
+                        shots.remove(shot)
+                        score += 1
 
             if radius <= 4:
                 end_round = True
@@ -168,6 +183,8 @@ def game():
                                    [300 + (radius + 2 * x) * math.cos(angle), 300 - (radius + 2 * x) * math.sin(angle)],
                                    1)
             shot[1] += 0.2
+            if shot[1] > 300:
+                shots.remove(shot)
 
         pygame.display.update()
 
