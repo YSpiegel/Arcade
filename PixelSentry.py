0222d628297fd5cafdd79cddd578e2000ca45042
YSpiegel <spiegel.yoav@gmail.com>
2022-09-28 16:32:44 +0300

    Added difficulties

--- PixelSentry.py
+++ PixelSentry.py
@@ -86,6 +86,24 @@ def game():
 
     score = 0
 
+    mode = -1
+
+    def generation_formula(x):
+        if mode == 0:
+            return 1.2
+        if mode == 1:
+            return 0.7 + 1/(x+1) + random.random()
+        if mode == 2:
+            return 0.1 + 1 / (0.03 * x + 0.5) + random.random()
+
+    def acceleration_factor(x):
+        if mode == 0:
+            return 0
+        if mode == 1:
+            return 0.00002
+        if mode == 2:
+            return 0.0002
+
     def pixel_scatter():
         while pixels_in_sight(pixels):
             dis.fill((255, 0, 0))
@@ -107,6 +125,39 @@ def game():
             pygame.display.update()
 
     while not game_over:
+
+        while mode == -1 and not game_over:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game_over = True
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x = pygame.mouse.get_pos()[0]
+                    y = pygame.mouse.get_pos()[1]
+                    if 100 <= x <= 500:
+                        if 150 <= y <= 250:
+                            mode = 0
+                        elif 300 <= y <= 400:
+                            mode = 1
+                        elif 450 <= y <= 550:
+                            mode = 2
+
+            dis.fill((255, 0, 0))
+            dis.blit(get_entry_text("Choose gamemode", 50), [90, 25])
+
+            # draw easy button
+            pygame.draw.rect(dis, black, [100, 150, 400, 100], width=10)
+            dis.blit(get_entry_text("Easy", 40), [260, 165])
+
+            # draw medium button
+            pygame.draw.rect(dis, black, [100, 300, 400, 100], width=10)
+            dis.blit(get_entry_text("Normal", 40), [240, 315])
+
+            # draw hardcore button
+            pygame.draw.rect(dis, black, [100, 450, 400, 100], width=10)
+            dis.blit(get_entry_text("Hardcore", 40), [220, 465])
+
+            pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
@@ -119,7 +170,7 @@ def game():
             sentry_angle = get_angle([300, 300], pygame.mouse.get_pos())
 
         # shoot a pixel if time has passed
-        if time.time() - last_attack > 0.1 + 1 / (0.03 * total_pixels + 0.5) + random.random():
+        if time.time() - last_attack > generation_formula(total_pixels):
             pixels.append([random.choice(numpy.arange(math.pi * 5 / 8, math.pi * 2 + math.pi * 3 / 8, math.pi / 32)),
                            300])
             last_attack = time.time()
@@ -138,7 +189,7 @@ def game():
             bottomx = 310 + radius * math.cos(angle)
             bottomy = 310 - radius * math.sin(angle)
             pygame.draw.rect(dis, black, [300 + radius * math.cos(angle), 300 - radius * math.sin(angle), 10, 10])
-            pixel[1] -= 0.05 + 0.002 * total_pixels / 10
+            pixel[1] -= 0.05 + acceleration_factor(total_pixels)
             for shot in shots:
                 if topx <= 300 + shot[1] * math.cos(shot[0]) <= bottomx \
                         and topy <= 300 - shot[1] * math.sin(shot[0]) <= bottomy and pixel in pixels:
@@ -175,14 +226,14 @@ def game():
 
             score = 0
 
+            mode = -1
+
         # create shots
         for shot in shots:
             angle = shot[0]
             radius = shot[1]
-            for x in range(1, 8):
-                pygame.draw.circle(dis, black,
-                                   [300 + (radius + 2 * x) * math.cos(angle), 300 - (radius + 2 * x) * math.sin(angle)],
-                                   1)
+            pygame.draw.line(dis, black, [300 + radius * math.cos(angle), 300 - radius * math.sin(angle)],
+                             [300 + (radius + 16) * math.cos(angle), 300 - (radius + 16) * math.sin(angle)], width=2)
             shot[1] += 0.2
             if shot[1] > 300:
                 shots.remove(shot)
