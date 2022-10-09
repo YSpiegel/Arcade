c7b4e241882e4ad2839d6c2f4421d84916883c77
YSpiegel <spiegel.yoav@gmail.com>
2022-10-09 10:58:39 +0300

    Added basic level template

--- BusinessMania/LevelTemplate.py
+++ BusinessMania/LevelTemplate.py
@@ -104,7 +104,7 @@ def horizontal_collusion(hitbox1, hitbox2):
         :return: The left hitbox or none if there's no collusion
         """
 
-    if hitbox1[1][0] <= hitbox2[1][0] <= hitbox1[1][1] or hitbox2[1][0] <= hitbox1[1][0] <= hitbox2[1][1]:
+    if hitbox1[1][0] <= hitbox2[1][0] + 1 <= hitbox1[1][1] or hitbox2[1][0] <= hitbox1[1][0] + 1 <= hitbox2[1][1]:
         if hitbox1[0][0] <= hitbox2[0][0] <= hitbox1[0][1]:
             return hitbox1
         if hitbox2[0][0] <= hitbox1[0][0] <= hitbox2[0][1]:
